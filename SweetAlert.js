a7c26f6be763a747fa753e96fde61debc62f4772
manufactura-t <zgfiodor@gmail.com>
2026-06-14 13:48:36 -0600

    Update SweetAlert.js

--- SweetAlert.js
+++ SweetAlert.js
@@ -14,3 +14,12 @@ function equipo(){
         html: "<img src='equipo1.jpeg' style='width: 100%'><img src='equipo2.jpeg' style='width: 100%'><img src='equipo3.jpeg' style='width: 100%'><img src='equipo4.jpeg' style='width: 100%'><img src='equipo5.jpeg' style='width: 100%'>"
     })
 }
+
+function datosCont(){
+    Swal.fire({
+        icon: "info",
+        theme: "dark",
+        title: "Datos de contacto",
+        html: '<span>Tel. B-IMAX: </span><a href="tel:+527225851077" style="color:white">7225851077</a> <br> <span>Email: </span><a href="mailto:ejc17099405@gmail.com" style="color: white">ejc17099405@gmail.com</a> <br> <span>Soporte Tecnico: </span><a href="mailto:zgfiodor@gmail.com" style="color: white">zgfiodor@gmail.com</a>'
+    })
+}
